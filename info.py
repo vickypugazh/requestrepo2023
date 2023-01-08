e4b3c3811913db33631f0e32342c897d0eca8775
tempreqbot <121566384+tempreqbot@users.noreply.github.com>
2023-01-08 14:30:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '5339620069:AAFwjfE7sDe4r64iMGMyxKea2vhJlSU
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/4d82a343859b34041095e.jpg https://telegra.ph/file/c7b2d31befc80493d1f2b.jpg https://telegra.ph/file/509cac221fc6da21d20e2.jpg https://telegra.ph/file/740cc35362085ec4e67d8.jpg https://telegra.ph/file/9559d95b8a112d031e3dd.jpg https://telegra.ph/file/dd57f6681b2de7c6e0455.jpg https://telegra.ph/file/8387d742e605e0de596b4.jpg https://telegra.ph/file/ce0eef39e66bed263eedf.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/10a1e021e8e17f1f9d748.jpg https://telegra.ph/file/13799b9867d818d462a91.jpg https://telegra.ph/file/284b9ae496a5588944939.jpg https://telegra.ph/file/7c835af37ce543e42bb40.jpg https://telegra.ph/file/111176e233ced491c23d3.jpg https://telegra.ph/file/3138930c17dbc2792c327.jpg https://telegra.ph/file/0746e2630a908d0ff1183.jpg https://telegra.ph/file/b932c2cf8370df145aec2.jpg https://telegra.ph/file/0838ea22886bf8824ae10.jpg https://telegra.ph/file/89a4c664ed74fc95d98e2.jpg')).split()
 NOR_IMG = environ.get('NOR_IMG', "https://telegra.ph/file/b8ed39d9ecf31a61ba47f.jpg")
 SPELL_IMG = environ.get('SPELL_IMG',"https://telegra.ph/file/a71f18535e38634eaa324.jpg")
 
