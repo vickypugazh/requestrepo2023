efcd4854361bda69f91ce865def66c0a73ba6a1c
tempreqbot <121566384+tempreqbot@users.noreply.github.com>
2023-01-11 00:37:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -784,7 +784,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.FIlESTORE_TXT,
+            text=script.FIlESTORE_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
